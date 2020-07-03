2b878b2aa32d76caa84ca45cc1422eb7e497f97b
ACCFernando <fernando.candalaft@gmail.com>
2020-07-03 19:25:49 -0300

    'pastas_e_doc'

---
+++ bolsa/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='O projeto da bolsa do Pingola e do Canjola',
+    author='Pingola e Canjola',
+    license='MIT',
+)
